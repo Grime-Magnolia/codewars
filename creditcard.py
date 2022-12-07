7b979b837aebe47d0c0fac1835017bd4654176f6
tygoz <81298529+tygoz@users.noreply.github.com>
2022-12-07 09:47:49 +0000

    fantastisch

--- creditcard.py
+++ creditcard.py
@@ -2,7 +2,7 @@ import random
 inp = list(input('-> '))
 leninp = len(inp)
 b = ''
-rand = random.randint(leninp-7,leninp-2)
+rand = leninp-4
 for a in range(leninp):
     if a <= rand:
         inp[a] = '#'
