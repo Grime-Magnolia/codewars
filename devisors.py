1162d6b8a542833cd8e6c7ed337965fcdfd777bd
tygoz <81298529+tygoz@users.noreply.github.com>
2022-12-08 07:45:29 +0000

    main

--- devisors.py
+++ devisors.py
@@ -3,11 +3,8 @@ def get(arg):
     b = []
     for a in range(1,max):
         a+=1
-        try:
-            c = arg/a
-            if is_integer(c) and c != 0 and c != 1 and a != arg:
-                b.append(arg/a)
-        except:
-            pass
+        c = arg/a
+        if type(c) == int and c != 0 and c != 1 and a != arg:
+            b.append(arg/a)
     return b
 print(get(int(input('->'))))
