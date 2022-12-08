a9fc98e14835564fab05d5a4eaa82322a4335f9c
tygoz <81298529+tygoz@users.noreply.github.com>
2022-12-08 07:37:00 +0000

    main

--- devisors.py
+++ devisors.py
@@ -1,1 +1,13 @@
-a = int(input('->'))
+max = 10000
+def get(arg):
+    b = []
+    for a in range(1,max):
+        a+=1
+        try:
+            c = arg/a
+            if is_integer(c) and c != 0 and c != 1 and a != arg:
+                b.append(arg/a)
+        except:
+            pass
+    return b
+print(get(int(input('->'))))
