262886bef429e8f5be3c905277ecad8a72bad5df
tygoz <81298529+tygoz@users.noreply.github.com>
2022-12-08 13:02:52 +0000

    main

--- devisors.py
+++ devisors.py
@@ -1,10 +1,10 @@
-max = 10000
+max = 100000
 def get(arg):
     b = []
-    for a in range(1,max):
+    for a in range(max):
         a+=1
-        c = arg/a
-        if type(c) == 'int' and c != 0 and c != 1 and a != arg:
-            b.append(arg/a)
+        c = float(arg/a)
+        if '.0 ' in str(c)+' ' and c != 0 and c != 1 and a != arg and c != arg:
+            b.append(int(arg/a))
     return b
 print(get(int(input('->'))))
