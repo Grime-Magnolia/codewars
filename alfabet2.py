d99aa62b5ba2b3f0dafce53056bc873fb78bf884
tygoz <81298529+tygoz@users.noreply.github.com>
2022-12-08 12:51:56 +0000

    rmed

--- alfabet2.py
+++
@@ -1,6 +0,0 @@
-a = list('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ \'1234567890-=_+`~!@#$$%%^&*(){}[]|\\;:\"\'/?<>,.')
-b = input('give your string\n: ')
-c = []
-for d in a:
-    b.replace(d,(' ',str(a.index(d)),' '))
-print(b)
