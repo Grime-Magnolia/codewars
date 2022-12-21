a8f8d7fb1cf850b6be360f881ba2eb3d6b5a031d
AbsoluteBeeHive <99733605+AbsoluteBeeHive@users.noreply.github.com>
2022-12-21 09:14:15 +0100

    IK HEB EEN COMMENT GEMAAKT VOOR TYGO
    op line 4 <-- (dit is hoe je een pullbericht maakt)

--- getmiddelchar.py
+++ getmiddelchar.py
@@ -1,6 +1,7 @@
 def get(arg):
     arg = list(arg)
     lenarg = len(arg)
+    #TYGO JE KUNT EEN VARIABLE NIET GEWOON 'A' OF 'B  NOEMEN!!!!!!!!!!!!!!!!!!!!'
     a = float(lenarg/2)
     if '.0 ' in str(a)+' ':
         return [arg[int(a-0.5)],arg[int(a+0.5)]]
