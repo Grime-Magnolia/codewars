f7cee81bf9fb56f1d901132ce58dd378097d6852
tygoz <81298529+tygoz@users.noreply.github.com>
2022-12-21 09:19:33 +0000

    made all code better and made better to read

--- getmiddelchar.py
+++ getmiddelchar.py
@@ -1,10 +1,19 @@
 def get(arg):
+
+    #make from string a list
     arg = list(arg)
+
+    #get len list
     lenarg = len(arg)
-    #TYGO JE KUNT EEN VARIABLE NIET GEWOON 'A' OF 'B  NOEMEN!!!!!!!!!!!!!!!!!!!!'
-    a = float(lenarg/2)
-    if '.0 ' in str(a)+' ':
-        return [arg[int(a-0.5)],arg[int(a+0.5)]]
+
+    #get middle of len list
+    middle_of_lenarg = float(lenarg/2)
+
+    #if str(middle_of_lenarg) is full int it will return the 2 middle chars
+    #else it will return the item arg
+    if '.0 ' in str(middle_of_lenarg):
+        return [arg[int(middle_of_lenarg-0.5)],arg[int(middle_of_lenarg+0.5)]]
     else:
-        return [arg[int(a)]]
+        return [arg[int(middle_of_lenarg)]]
+
 print(get(input('~>')))
