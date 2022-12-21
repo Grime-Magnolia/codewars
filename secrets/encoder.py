5921acb9932baec7272c271730696e2796e255dc
tygoz <81298529+tygoz@users.noreply.github.com>
2022-12-21 09:34:45 +0000

    nothing much

---
+++ secrets/encoder.py
@@ -0,0 +1,6 @@
+class encode():
+    alfabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%%^&*()'
+    allkeys = {}
+    for num,loop in enumerate(alfabet):
+        allkeys{loop} = num
+    def make(str,key)
