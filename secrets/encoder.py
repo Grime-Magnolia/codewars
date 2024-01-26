95cd55c40f845f1ab6f78d26c682e3b7d4dab853
tygoz <81298529+tygoz@users.noreply.github.com>
2024-01-26 16:30:30 +0100

    Update encoder.py it is cypher

--- secrets/encoder.py
+++ secrets/encoder.py
@@ -2,7 +2,7 @@ import random
 class encode():
     def __init__(self):
         self.alfabet = '6]ú0nfæ+»gFVK;yáéx!lT#^U{«q[%,G}Q4R&m27Aa%dpäðßzDe(ü-1þµí/8¥þS=>Lö¶¿btohJ×H®_.\"OCB<?NñwµIMå9)ç|ZiX3PWusvø$:Y5óc\'*¬Ejr@k'
-        self.allkeys = {}
+        self.allkeys = {y:x for x,y in enumerate(self.alfabet)}
         for self.num,loop in enumerate(self.alfabet):
             self.allkeys[loop] = self.num
     def make(self,string1,yourkey):
